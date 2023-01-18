d8fe667665c0a39cbfdc86fcea4b82c8813934f8
komalarao <manoharvakamullu1999@gmail.com>
2023-01-18 00:45:31 -0800

    added: initial stake to gambling game

---
+++ src/com/bridgelabz/gamblingSimulator/GamblingGame.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.gamblingSimulator;
+
+public class GamblingGame {
+
+	public static final int initialStake = 100;
+	public static final int stakeBet = 1;
+
+	public static void main(String[] args) {
+
+		System.out.println("Initial Stake is: " + initialStake + "rupees");
+		System.out.println("Stake Bet is " + stakeBet);
+
+	}
+
+}
